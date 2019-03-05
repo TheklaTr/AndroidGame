fbf86ab9f57f58ef13dd3a38e56bf8bb826ef405
TrHien <ttthuhien0104@gmail.com>
2019-03-05 22:21:44 +0200

    Add files

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float maxHorizontalSpeed;
+    public float vertSpeed;
+
+    private Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        rigid.velocity = new Vector2(vertSpeed, maxHorizontalSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float speed;
+
+    public float direction;
+    public float adjustSpeed;
+
+    public Transform rightLimit ;
+    public Transform leftLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime,
+                transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime,
+                transform.position.y, transform.position.z);
+        }
+        else
+            direction = 0;
+
+        if (transform.position.x > rightLimit.position.x)
+        {
+            transform.position = new Vector3(rightLimit.position.x, 
+                transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < leftLimit.position.x)
+        {
+            transform.position = new Vector3(leftLimit.position.x,
+                transform.position.y, transform.position.z);
+        }
+    }
+
+    // stop moving when touch the walls 
+    private void OnCollisionEnxit2D(Collision2D other)
+    {
+        other.rigidbody.velocity = new Vector2(other.rigidbody.velocity.x + direction * adjustSpeed,
+                            other.rigidbody.velocity.y);
+        Debug.Log(other.rigidbody.velocity);
+    }
+}
